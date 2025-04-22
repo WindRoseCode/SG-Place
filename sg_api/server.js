6c83ce1865dae2e55ce1761d9b9c6626dcf01e8b
Anthony Medeiros <103971203+AnthonyMedr@users.noreply.github.com>
2025-04-22 20:06:00 -0300

    Configurações básicas do servidor http

---
+++ sg_api/server.js
@@ -0,0 +1,77 @@
+'use strict'
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+const express = require('express');
+const { time } = require('console');
+const { title } = require('process');
+const { version } = require('os');
+
+ const app = express();
+ const port = normalizePort(process.env.PORT || '3000');
+ app.set('port',port);
+
+ const server = http.createServer(app);
+ const router = express.Router();
+
+ const route = router.get('/',(req,res,next) =>{
+    res.status(200).send({
+        title: " Sg Web API",
+        version: "0.0.1"
+    });
+ });
+ app.use('/',route);
+
+ server.listen(port);
+ server.on('error', onError);
+
+ console.log('API rodando na porta ' + port);
+
+
+ //função para normalizar a porta.
+ function normalizePort(val){
+    const port = parseInt(val,10);
+
+    if(isNaN(port)){
+        return val;
+    }
+    if(port >= 0){
+        return port;
+    }
+    return false;
+ }
+//Função para tratar erros comuns
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string'
+        ? 'Pipe ' + port
+        : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        case 'ECONNREFUSED':
+            console.error(bind + ' connection was refused');
+            process.exit(1);
+            break;
+        case 'EHOSTUNREACH':
+            console.error(bind + ' host is unreachable');
+            process.exit(1);
+            break;
+        case 'ETIMEDOUT':
+            console.error(bind + ' connection timed out');
+            process.exit(1);
+            break;
+        default:
+            console.error('Unexpected error: ', error);
+            throw error;
+    }
+}
